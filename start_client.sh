6179f6467af0b1ba1e6253900d2c637803b9d363
Olivier R-D <olivier@dummy.com>
2014-03-28 14:07:06 +0100

    add some missing dep, added new files to cmake

---
+++ start_client.sh
@@ -0,0 +1,1 @@
+./build/client/client --uri opc.tcp://192.168.56.101:48030 --browse --node-id-string Makino02 --namespace-index 2 --config-dir client/config
---
+++ start_server.sh
@@ -0,0 +1,2 @@
+set +x
+build/server/server --config server/config/ --log-file=server.log
