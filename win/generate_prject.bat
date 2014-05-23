a20c2f4866799b3d6b551f48ac4942fb3f770042
olivier <olivier@windows7>
2014-05-23 15:19:30 +0200

    cmake project generation seems to work

---
+++ win/generate_prject.bat
@@ -0,0 +1,10 @@
+
+set PATH=%PATH%;C:\libxml2\libxml2-2.7.8.win32\lib
+rem set BOOST_ROOT=C:\boost_1_55_0\boost
+rem set BOOST_INCLUDEDIR=C:\boost_1_55_0\boost
+set BOOST_LIBRARYDIR=C:\boost_1_55_0\lib64-msvc-12.0
+del CMakeCache.txt
+del *.vcx*
+del *.sln*
+cmake -G "Visual Studio 12" ..
+rem cmake --build . --target ALL_BUILD --config Debug
